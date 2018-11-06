37ed4ebe83a379946cb28e5fd0bc7f989ffcb991
maximo <maximodleon@gmail.com>
2018-11-05 21:36:34 -0400

    post file to server for conversion, get converted file from server and save

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { saveAs } from 'file-saver'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { selectedFile: null, loaded: 0 }
+    this.hadnleSelectedFile = this.hadnleSelectedFile.bind(this)
+    this.handleUpload = this.handleUpload.bind(this)
+ }
+
+ hadnleSelectedFile(event) {
+         console.log('selectedFile', event.target.files[0])
+         this.setState({
+                 selectedFile: event.target.files[0],
+                 loaded: 0
+         })
+ } 
+
+  handleUpload() {
+    const data = new FormData()
+    data.append('file', this.state.selectedFile,
+            this.state.selectedFile.name)
+
+    fetch('http://localhost:8080/upload?from=pdf&to=pdf', {
+      method: 'POST',
+      body: data
+    })
+                  .then((data) => { 
+                          console.log('data', data.headers)
+                         return data.blob() 
+                  
+    })
+    .then((blob) => saveAs(blob, 'thisfile.pdf'))
+    .catch((error) => console.log('error', error))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <React.Fragment>
+            <input type="file" name="filepond" id="filepond" onChange={this.hadnleSelectedFile}/>
+            <button onClick={this.handleUpload}>Upload </button>
+         </React.Fragment>
+        
     );
   }
 }
